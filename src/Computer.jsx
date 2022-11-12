ef4361779945b0dff7b4d78a5fdf4e9ac89f1b8b
Tung Teng <thetungctn@gmail.com>
2022-11-13 04:43:42 +0700

    Loading portfolio site to computer screen

--- src/Computer.jsx
+++ src/Computer.jsx
@@ -1,10 +1,24 @@
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, Html } from "@react-three/drei";
 
 const Computer = () => {
   /* Macbook model downloaded from https://market.pmnd.rs/model/macbook */
   const computer = useGLTF("./macbook.gltf");
 
-  return <primitive object={computer.scene} position-y={-1.2} />;
+  return (
+    <primitive object={computer.scene} position-y={-1.2}>
+      <Html
+        transform
+        distanceFactor={1}
+        position={[0, 1.56, -1.4]}
+        rotation-x={-0.256}
+      >
+        <iframe
+          className="portfolio"
+          src="https://tung-portfolio-challenge.netlify.app/"
+        />
+      </Html>
+    </primitive>
+  );
 };
 
 export default Computer;
