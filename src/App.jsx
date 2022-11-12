f5e656c7fa6f0f6288139e2f1c6ceb6c8804916d
Tung Teng <thetungctn@gmail.com>
2022-11-13 03:02:08 +0700

    Install r3f + drei & Setup basic scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <Canvas camera={{ fov: 45, position: [-3, 1.5, 4] }}>
+      <OrbitControls />
+
+      <mesh>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
+    </Canvas>
+  );
 }
 
 export default App;
