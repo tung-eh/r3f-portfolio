ee70e2408bc036eb9a6e2117cb26c88293fb407e
Tung Teng <thetungctn@gmail.com>
2022-11-13 04:43:08 +0700

    Limit controls from user

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, Environment, Float } from "@react-three/drei";
+import {
+  PresentationControls,
+  Environment,
+  ContactShadows,
+  Float,
+} from "@react-three/drei";
 import { useControls } from "leva";
 import Computer from "./Computer";
 
@@ -12,11 +17,21 @@ const ThreeScene = () => {
     <>
       <Environment preset="city" />
       <color args={[bgColor]} attach="background" />
-      <OrbitControls />
 
-      <Float floatIntensity={0.4}>
-        <Computer />
-      </Float>
+      <PresentationControls
+        global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}
+        snap={{ mass: 4, tension: 400 }}
+      >
+        <Float floatIntensity={0.4}>
+          <Computer />
+        </Float>
+      </PresentationControls>
+
+      <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
     </>
   );
 };
